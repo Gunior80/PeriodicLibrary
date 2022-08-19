73ad9cfebfadb63513ea93022a91da96461b1808
gunior80 <gunior80@yandex.ru>
2022-08-19 23:40:01 +0300

    added find or create

--- initdata.py
+++ initdata.py
@@ -5,24 +5,27 @@ from django.core.files import File
 from django.utils.translation import gettext_lazy as _
 from datetime import datetime as dt
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PeriodicLibrary.settings")
 
 application = get_wsgi_application()
 
 from library.models import Periodical, Instance
 
-
 if __name__ == "__main__":
-    root = pathlib.Path('d:\\Няръяна Вындер')
+    root = pathlib.Path('E:\\Няръяна Вындер')
     files = root.rglob('*.pdf')
-    periodic = Periodical(name=root.name)
-    periodic.save()
+
+    periodic, created = Periodical.objects.get_or_create(name=root.name)
+    if created:
+        periodic.save()
     for file in files:
-        with open(file, 'rb') as f:
-            instance = Instance(periodical=periodic, file=File(f),
-                                date=dt.strptime(file.stem[3:-2]+file.parent.name, "%d.%m.%Y"))
-            instance.save()
-            instance.tags.add(instance.date.strftime('%Y'),
-                              str(_(instance.date.strftime('%B'))),
-                              instance.date.strftime("%d"))
+        instance, created = Instance.objects.get_or_create(periodical=periodic,
+                                                           date=dt.strptime(file.stem[3:-2] + file.parent.name,
+                                                                            "%d.%m.%Y"))
+        if created:
+            with open(file, 'rb') as f:
+                instance.file = File(f)
+                instance.save()
+                instance.tags.add(instance.date.strftime('%Y'),
+                                  str(_(instance.date.strftime('%B'))),
+                                  instance.date.strftime("%d"))
